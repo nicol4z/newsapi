b9d840a75dba8cad29bf3742a5e9e5e86cf0ee99
Nicolas Barrios <nicolas.barrios@alumnos.ucn.cl>
2021-12-03 17:42:37 -0300

    chore(Project)= initialize

---
+++ src/test/java/cl/ucn/disc/dsm/nbarrios/newsapi/NewsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ucn.disc.dsm.nbarrios.newsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
